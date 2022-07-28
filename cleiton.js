bac9bd5689243925f66f9605b332e28071d7bcc1
SirKleyton <cleyton_ferr@hotmail.com>
2022-07-28 13:40:55 -0300

    1 file modified

--- cleiton.js
+++ cleiton.js
@@ -1,6 +1,17 @@
-const meuConjunto = new Set();
+/*const meuConjunto = new Set();
 
 const times = ['São Paulo', 'Flamengo', 'Fluminense', 'Palmeiras', 'São Paulo'];
 const meuCampeonato = new Set(times);
 
-console.table(meuCampeonato);
+console.table(meuCampeonato);*/
+
+const meuConjunto = new Set();
+
+meuConjunto.add('Cleiton');
+meuConjunto.add('Cleiton');
+meuConjunto.add('1986');
+meuConjunto.add(true);
+meuConjunto.add(true);
+meuConjunto.add(['Cleiton', 'Marcia']);
+meuConjunto.add(Casal);
+console.table(meuConjunto);
